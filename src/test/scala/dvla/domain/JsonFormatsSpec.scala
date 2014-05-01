c70fbdb5035e16ae26988c6d32a2794d0883483d
Geraint Jones <geraint@Geraints-MacBook-Air.local>
2014-05-01 10:28:49 +0100

    added uprn to address tests

--- src/test/scala/dvla/domain/JsonFormatsSpec.scala
+++ src/test/scala/dvla/domain/JsonFormatsSpec.scala
@@ -3,20 +3,28 @@ package dvla.domain
 import org.scalatest.{Matchers, WordSpec}
 import dvla.domain.JsonFormats._
 import spray.json._
-import dvla.domain.address_lookup.PostcodeToAddressLookupRequest
+import dvla.domain.address_lookup.{UprnToAddressLookupRequest, PostcodeToAddressLookupRequest}
 
 class JsonFormatsSpec extends WordSpec with Matchers {
 
   "JsonFormats" should {
-    "successfully unmarshall a valid json address lookup request payload into a request object" in {
+
+    "successfully unmarshall a valid json postocde to address lookup request payload into a request object" in {
       val expectedRequest = PostcodeToAddressLookupRequest("SA11AA")
       val jsonPayload = """{"postcode":"SA11AA"}"""
       val unmarshalledRequest = jsonPayload.asJson.convertTo[PostcodeToAddressLookupRequest]
       unmarshalledRequest should equal(expectedRequest)
     }
 
+    "successfully unmarshall a valid json uprn to address lookup request payload into a request object" in {
+      val expectedRequest = UprnToAddressLookupRequest(12345L)
+      val jsonPayload = """{"uprn":12345}"""
+      val unmarshalledRequest = jsonPayload.asJson.convertTo[UprnToAddressLookupRequest]
+      unmarshalledRequest should equal(expectedRequest)
+    }
+
   }
 
-  //TODO tests for other two json formats
+  //TODO tests for other json formats
 
 }
--- src/test/scala/dvla/microservice/QASAddressLookupSpec.scala
+++ src/test/scala/dvla/microservice/QASAddressLookupSpec.scala
@@ -2,29 +2,58 @@ package dvla.microservice
 
 import dvla.domain.JsonFormats._
 import spray.http.StatusCodes._
-import dvla.domain.address_lookup.PostcodeToAddressResponse
+import dvla.domain.address_lookup.{AddressViewModel, UprnToAddressResponse, PostcodeToAddressResponse}
 
 class QASAddressLookupSpec extends RouteSpecBase {
 
   val postcodeValid = "SA11AA"
-  val addressLookupUrl = "/postcode-to-address"
+  val uprnValid = 12345L
+  val postocdeToAddressLookupUrl = "/postcode-to-address"
+  val uprntoAddressLookupUrl = "/uprn-to-address"
 
-  "The address lookup service" should {
+
+  private def testValidAddressViewModel(addressViewModel: AddressViewModel) = {
+    addressViewModel.uprn.get should equal(12345)
+    //TODO test address parts of the the model
+  }
+
+
+  "The postcode to address lookup service" should {
 
     // This is used because you can't create real request object with invalid parameters
     case class TestRequest(postcode: String)
 
     implicit val testRequestFormat = jsonFormat1(TestRequest)
 
-    "return a successful response containing a model for a valid address lookup request" in {
+    "return a successful response containing a model for a valid postcode to address lookup request" in {
       val request = TestRequest(postcodeValid)
-      Post(addressLookupUrl, request) ~> sealRoute(route) ~> check {
+      Post(postocdeToAddressLookupUrl, request) ~> sealRoute(route) ~> check {
         status should equal(OK)
         val resp = responseAs[PostcodeToAddressResponse]
-        resp.addresses(0).uprn should equal ("12345")
+        resp.addresses(0).uprn should equal("12345")
         // TODO check address half of pair after using mocked not canned data
       }
     }
 
   }
+
+  "The uprn to address lookup service" should {
+
+    // This is used because you can't create real request object with invalid parameters
+    case class TestRequest(uprn: Long)
+
+    implicit val testRequestFormat = jsonFormat1(TestRequest)
+
+    "return a successful response containing a model for a valid uprn to address lookup request" in {
+      val request = TestRequest(uprnValid)
+      Post(uprntoAddressLookupUrl, request) ~> sealRoute(route) ~> check {
+        status should equal(OK)
+        val resp = responseAs[UprnToAddressResponse]
+        resp.addressViewModel should be (defined)
+        testValidAddressViewModel(resp.addressViewModel.get)
+      }
+    }
+
+  }
+
 }
