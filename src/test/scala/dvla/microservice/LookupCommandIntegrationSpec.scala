e252875de6d2e3b140f5cac83aec64ffdf81c463
Oliver Jenkins <oliver.jenkins@dvla.gsi.gov.uk>
2015-09-07 16:50:08 +0100

    US2529 Re-instated unit test against live os-lookup service

--- src/test/scala/dvla/microservice/LookupCommandIntegrationSpec.scala
+++ src/test/scala/dvla/microservice/LookupCommandIntegrationSpec.scala
@@ -1,6 +1,7 @@
 package dvla.microservice
 
 import com.typesafe.config.{ConfigFactory, Config}
+import dvla.common.clientsidesession.TrackingId
 import dvla.domain.address_lookup.{UprnToAddressLookupRequest, PostcodeToAddressLookupRequest}
 import dvla.helpers.UnitSpec
 import dvla.microservice.ordnance_survey_preproduction._
@@ -11,48 +12,41 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class LookupCommandIntegrationSpec extends UnitSpec{
 
-//  private val validUprn = 10010030533L
-//  private val validPostcode = "SA11AA"
-//
-//  private val conf = ConfigFactory.load()
-//  private val configuration = new Configuration(
-//    baseUrl = conf.getString("ordnancesurvey.override.baseurl"),
-//    apiKey = conf.getString("ordnancesurvey.preproduction.apikey"))
-//
-//  "call os address lookup real endpoint" should {
-//
-//    def testConfig: Config = {
-//      ConfigFactory.empty().withFallback(ConfigFactory.load())
-//    }
-//
-//    implicit val system = ActorSystem("LookupCommandSpecPreProduction", testConfig)
-//
-//    val command = {
-//      val callOrdnanceSurvey = {
-//        val postcodeUrlBuilder = new PostcodeUrlBuilder(configuration = configuration)
-//        val uprnUrlBuilder = new UprnUrlBuilder(configuration = configuration)
-//        new CallOrdnanceSurveyImpl(postcodeUrlBuilder, uprnUrlBuilder)
-//      }
-//      new ordnance_survey_preproduction.LookupCommand(
-//        configuration = configuration,
-//        callOrdnanceSurvey = callOrdnanceSurvey
-//      )
-//    }
-//
-//    "return a valid postcode response" in {
-//      val result = command(PostcodeToAddressLookupRequest(validPostcode))
-//
-//      whenReady(result, Timeout(Span(10, Seconds))) { r =>
-//        r.addresses.foreach(a => a.uprn should equal(validUprn.toString))
-//      }
-//    }
-//    "return a valid uprn response" in {
-//      val result = command(UprnToAddressLookupRequest(validUprn))
-//
-//      whenReady(result, Timeout(Span(10, Seconds))) { r =>
-//        r.addressViewModel.foreach(a => a.uprn.getOrElse(0) should equal(validUprn))
-//      }
-//    }
-//  }
 
+  private val validUprn = 10033544614L
+  private val validPostcode = "SW1A1AA"
+
+  private val conf = ConfigFactory.load()
+  private val configuration = new Configuration(
+    baseUrl = conf.getString("ordnancesurvey.override.baseurl"),
+    apiKey = conf.getString("ordnancesurvey.preproduction.apikey"))
+
+  "call os address lookup using real endpoint" should {
+    def testConfig: Config = {
+      ConfigFactory.empty().withFallback(ConfigFactory.load())
+    }
+
+    implicit val system = ActorSystem("LookupCommandSpecPreProduction", testConfig)
+    implicit val trackingId = TrackingId("test-tracking-id")
+
+    val command = {
+      val callOrdnanceSurvey = {
+        val postcodeUrlBuilder = new PostcodeUrlBuilder(configuration = configuration)
+        val uprnUrlBuilder = new UprnUrlBuilder(configuration = configuration)
+        new CallOrdnanceSurveyImpl(postcodeUrlBuilder, uprnUrlBuilder)
+      }
+      new ordnance_survey_preproduction.LookupCommand(
+        configuration = configuration,
+        callOrdnanceSurvey = callOrdnanceSurvey
+      )
+    }
+
+    "return a valid postcode response for Buckingham Palace" in {
+      val result = command(PostcodeToAddressLookupRequest(validPostcode))
+
+      whenReady(result, Timeout(Span(10, Seconds))) { r =>
+        r.addresses.foreach(a => a.uprn should equal(validUprn.toString))
+      }
+    }
+  }
 }
